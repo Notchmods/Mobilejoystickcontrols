93b3e2f4127d497aab6dd1406ed0d136ec8a40b5
Notchmods <Nomics3000@gmail.com>
2022-01-25 09:11:21 +1100

    FPS Joystick control project

---
+++ Unitymobilejoystick/Assets/Camerapanning.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camerapanning : MonoBehaviour
+{
+    public float SpeedH = 2f;
+    public float SpeedV = 2f;
+    public float yaw;
+    public float pitch;
+    public Joystick Lookaroundjoystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Assign the panning and looking function to a joystick
+        yaw -= SpeedH * Lookaroundjoystick.Vertical;
+        pitch += SpeedV * Lookaroundjoystick.Horizontal;
+        transform.eulerAngles = new Vector3(yaw, pitch, 0f);
+
+        
+    }
+}
---
+++ Unitymobilejoystick/Assets/Playermovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public Joystick Joystick1;
+    public Transform Maincamera;
+    public CharacterController Bodymovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Assign the joystick to a variable
+        float Horizontalstick = Joystick1.Horizontal;
+        float Verticalstick = Joystick1.Vertical;
+        Vector3 camF = Maincamera.forward;
+        Vector3 Camy = Maincamera.right;
+        camF.y = 0;
+        Camy.y = 0;
+        //Make sure that the character move forward at the direction of where the camera is facing
+        var Movingdirection = camF * Verticalstick + Camy * Horizontalstick;
+        Bodymovement.Move(Movingdirection * 15f * Time.deltaTime);
+
+    }
+}
